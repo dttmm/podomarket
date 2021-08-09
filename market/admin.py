ae9376cf839864311d01131bc4f260a3c5ed93e6
DESKTOP-2R119A6\snrn <vkdnjxodnd@naver.com>
2021-08-09 17:49:22 +0900

    커스텀필드 추가 (nickname, kakao_id, address)

--- market/admin.py
+++ market/admin.py
@@ -4,3 +4,7 @@ from .models import User
 
 # Register your models here.
 admin.site.register(User, UserAdmin)
+UserAdmin.fieldsets += ("Custom fields",
+                        {"fields": ("nickname", 'kakao_id', 'address',)}),
+# UserAdmin.fieldsets += ('Custom fields',
+#                         {'fields': ('nickname', 'kakao_id', 'address',)}),
