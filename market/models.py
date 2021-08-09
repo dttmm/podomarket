d2aa2e84647491d2e2acf68241cc8fe614d9057a
DESKTOP-2R119A6\snrn <vkdnjxodnd@naver.com>
2021-08-09 18:39:50 +0900

    회원가입 필드에 validators 적용함

--- market/models.py
+++ market/models.py
@@ -1,12 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from .validators import *
 # Create your models here.
 
 
 class User(AbstractUser):
-    nickname = models.CharField(max_length=15, unique=True, null=True)
-    kakao_id = models.CharField(max_length=20, null=True)
-    address = models.CharField(max_length=40, null=True)
+    nickname = models.CharField(
+        max_length=15,
+        unique=True,
+        null=True,
+        error_messages={'unique': "이미 사용중인 닉네임입니다."},
+        validators=[validate_no_special_characters]
+    )
+
+    kakao_id = models.CharField(
+        max_length=20,
+        null=True,
+        validators=[validate_no_special_characters]
+    )
+
+    address = models.CharField(
+        max_length=40,
+        null=True,
+        validators=[validate_no_special_characters]
+    )
 
     def __str__(self):
         return self.email
--- podomarket/settings.py
+++ podomarket/settings.py
@@ -93,16 +93,7 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'market.validators.CustomPasswordValidator',
     },
 ]
 
@@ -110,7 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko'
 
 TIME_ZONE = 'UTC'
 
@@ -144,5 +135,6 @@ ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_SESSION_REMEMBER = True
 ACCOUNT_SIGNUP_FORM_CLASS = "market.forms.SignupForm"
+ACCOUNT_PASSWORD_INPUT_RENDER_VALUE = True
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
