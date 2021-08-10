f6d109ac2c5ebe656e8237c3824d3c3df2337f14
DESKTOP-2R119A6\snrn <vkdnjxodnd@naver.com>
2021-08-10 09:33:12 +0900

    이메일 인증 완료페이지 추가

--- podomarket/urls.py
+++ podomarket/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('', include('market.urls')),
+
+    path('email-confirmation-done/', TemplateView.as_view(
+        template_name='market/email-confirmation-done.html'
+    ), name='account_email_confirmation_done'),
     path('', include('allauth.urls')),
 ]
