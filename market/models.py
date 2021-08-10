ce14020acbfb2512962b86281a4d94f22717a6e1
DESKTOP-2R119A6\snrn <vkdnjxodnd@naver.com>
2021-08-10 18:50:44 +0900

    Post 모델 추가

--- market/models.py
+++ market/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.deletion import CASCADE
 from .validators import *
 # Create your models here.
 
@@ -27,3 +28,28 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=60)
+    item_price = models.IntegerField()
+
+    CONDITION = [
+        (1, '새제품'),
+        (2, '최상'),
+        (3, '상'),
+        (4, '중'),
+        (5, '하'),
+    ]
+
+    item_condition = models.IntegerField(choices=CONDITION)
+    item_details = models.TextField(null=True)
+    image1 = models.ImageField(upload_to='item_pics')
+    image2 = models.ImageField(upload_to='item_pics', blank=True)
+    image3 = models.ImageField(upload_to='item_pics', blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    dt_created = models.DateTimeField(auto_now_add=True)
+    dt_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
--- podomarket/urls.py
+++ podomarket/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 from market.views import CustomPasswordChangeView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,3 +32,5 @@ urlpatterns = [
          name='account_password_change'),
     path('', include('allauth.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
