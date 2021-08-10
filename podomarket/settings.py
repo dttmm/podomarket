7436164543ac3136154e9ba6cc0c182653663903
DESKTOP-2R119A6\snrn <vkdnjxodnd@naver.com>
2021-08-10 17:07:39 +0900

    템플ㄹ시 오버라이딩, 디자인 꾸밈

--- podomarket/settings.py
+++ podomarket/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'market',
     'django.contrib.sites',
+    'widget_tweaks',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
