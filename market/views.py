d5dcb6296472b5d060ffb5eac0897feb6d7920a9
DESKTOP-2R119A6\snrn <vkdnjxodnd@naver.com>
2021-08-11 13:59:29 +0900

    제네릭 DetailView로 상세페이지 구현

--- market/views.py
+++ market/views.py
@@ -1,14 +1,10 @@
 from django.shortcuts import render
 from allauth.account.views import PasswordChangeView
 from django.urls import reverse
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # Create your views here.
-
-
-def index(request):
-    return render(request, 'market/index.html')
 
 
 class IndexView(ListView):
@@ -17,6 +13,12 @@ class IndexView(ListView):
     context_object_name = 'posts'
     paginate_by = 8
     ordering = ['-dt_created']
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'market/post_detail.html'
+    pk_url_kwarg = 'post_id'
 
 
 class CustomPasswordChangeView(PasswordChangeView):
