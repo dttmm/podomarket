65e175022cf31afcdbc60ca07912f532072e88aa
DESKTOP-2R119A6\snrn <vkdnjxodnd@naver.com>
2021-08-11 13:31:09 +0900

    제네릭 Listview로 홈페이지 구현

--- market/views.py
+++ market/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 from allauth.account.views import PasswordChangeView
 from django.urls import reverse
+from django.views.generic import ListView
+from .models import Post
 
 # Create your views here.
 
 
 def index(request):
     return render(request, 'market/index.html')
+
+
+class IndexView(ListView):
+    model = Post
+    template_name = 'market/index.html'
+    context_object_name = 'posts'
+    paginate_by = 8
+    ordering = ['-dt_created']
 
 
 class CustomPasswordChangeView(PasswordChangeView):
--- podomarket/settings.py
+++ podomarket/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'market',
     'django.contrib.sites',
     'widget_tweaks',
